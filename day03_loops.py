5fecc6d8e545eafc861787bd83b62abbdc2d6aa7
Harsh Gupta <141241562+Harsh0466@users.noreply.github.com>
2026-04-05 19:54:36 +0530

    Add sum of first n numbers and multiplication table
    Added functionality to sum the first n numbers and print a multiplication table.

--- day03_loops.py
+++ day03_loops.py
@@ -8,3 +8,19 @@ for i in range(1, 11):
 # 2. Print even numbers from 1 to 20
 for i in range(2, 21, 2):
     print(i)
+
+# 3. Sum of first n numbers
+n = int(input("Enter the number"))
+okay = 0
+
+for i in range(1, n+1):
+    okay = okay + i
+
+print(okay)
+
+
+# 4. Multiplication table
+n = int(input("Enter the number"))
+
+for i in range(1, 11):
+    print(n, "x", i, '=', n * i)
