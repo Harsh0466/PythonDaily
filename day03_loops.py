5c3e6bdfd5d13c4a946b7680aed2c0dbd6d00c86
Harsh Gupta <141241562+Harsh0466@users.noreply.github.com>
2026-04-06 19:10:34 +0530

    Implement factorial calculation in day03_loops.py
    Add factorial calculation for a given number.

--- day03_loops.py
+++ day03_loops.py
@@ -24,3 +24,12 @@ n = int(input("Enter the number"))
 
 for i in range(1, 11):
     print(n, "x", i, '=', n * i)
+
+# 5. Factorial of a Number
+n = int(input("Enter the number"))
+total = 1
+
+for i in range(1, n+1):
+    total = total * i
+    
+print(total)
